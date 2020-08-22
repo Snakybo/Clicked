ddbaee309ae64f2748fa47dcce717f74cc106c85
Snakybo <krol.kevin@gmail.com>
2020-08-22 22:55:51 +0200

    Include mousewheel up and down in list sorting

--- BindingConfig.lua
+++ BindingConfig.lua
@@ -6,7 +6,7 @@ local GetSpellInfo = GetSpellInfo
 local InCombatLockdown = InCombatLockdown
 
 local keybindOrderMapping = {
-	"BUTTON1", "BUTTON2", "BUTTON3", "BUTTON4", "BUTTON5",
+	"BUTTON1", "BUTTON2", "BUTTON3", "BUTTON4", "BUTTON5", "MOUSEWHEELUP", "MOUSEWHEELDOWN",
 	"`", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "=",
 	"NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "NUMPADDIVIDE", "NUMPADMULTIPLY", "NUMPADMINUS", "NUMPADPLUS", "NUMPADDECIMAL",
 	"F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13",
