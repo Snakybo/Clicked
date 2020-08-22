f06cf7394ca242a8beadcc4d5dc890cb58181934
Snakybo <krol.kevin@gmail.com>
2020-08-22 16:14:08 +0200

    Remove debug dump function

--- Clicked.lua
+++ Clicked.lua
@@ -638,16 +638,3 @@ function Clicked:CanTargetUnitBeHostile(unit)
 
 	return false
 end
-
-function dump(o)
-	if type(o) == 'table' then
-	   local s = '{ '
-	   for k,v in pairs(o) do
-		  if type(k) ~= 'number' then k = '"'..k..'"' end
-		  s = s .. '['..k..'] = ' .. dump(v) .. ','
-	   end
-	   return s .. '} '
-	else
-	   return tostring(o)
-	end
- end
