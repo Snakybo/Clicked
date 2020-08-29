cd8377ea5f2015978d333a1b46a20882ec7620d7
Snakybo <krol.kevin@gmail.com>
2020-08-30 01:11:47 +0200

    Add Blizzard compact raid frame integration

--- UnitFrames/Blizzard.lua
+++ UnitFrames/Blizzard.lua
@@ -26,10 +26,37 @@ local BLIZZARD_UNIT_FRAMES = {
 	}
 }
 
+local function HookCompactUnitFramePart(name, part, index)
+	local frame = _G[name .. part .. index]
+
+	if frame ~= nil then
+		Clicked:RegisterClickCastFrame("", frame)
+	end
+end
+
+local function HookCompactUnitFrame(frame, ...)
+	if frame == nil or frame:IsForbidden() then
+		return
+	end
+
+	local name = frame:GetName()
+
+	for i = 1, 3 do
+		HookCompactUnitFramePart(name, "Buff", i)
+		HookCompactUnitFramePart(name, "Debuff", i)
+		HookCompactUnitFramePart(name, "DispelDebuff", i)
+		HookCompactUnitFramePart(name, "CenterStatusIcon", i)
+	end
+
+	Clicked:RegisterClickCastFrame("", frame)
+end
+
 function Clicked:RegisterBlizzardUnitFrames()
 	for addon, names in pairs(BLIZZARD_UNIT_FRAMES) do
 		for _, name in ipairs(names) do
 			self:RegisterClickCastFrame(addon, name)
 		end
 	end
+
+	hooksecurefunc("CompactUnitFrame_SetUpFrame", HookCompactUnitFrame)
 end
