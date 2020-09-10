3898bdadcc5e9abc809f75b8e8c260baf72377c6
Snakybo <krol.kevin@gmail.com>
2020-09-10 22:29:15 +0200

    Disable bindings in vehicles, pet battles, and mind controls

--- Core/CommandProcessor.lua
+++ Core/CommandProcessor.lua
@@ -11,9 +11,9 @@ local function GetCommandAttributeIdentifier(command, isClickCastCommand)
 
 	-- remove any trailing dashes (shift- becomes shift, ctrl- becomes ctrl, etc.)
 	if prefix:sub(-1, -1) == "-" then
-        prefix = prefix:sub(1, -2)
-    end
-	
+		prefix = prefix:sub(1, -2)
+	end
+
 	-- convert the parts to lowercase so it fits the attribute naming style
 	prefix = prefix:lower()
 	suffix = suffix:lower()
@@ -31,9 +31,71 @@ local function GetCommandAttributeIdentifier(command, isClickCastCommand)
 	return prefix, suffix, buttonIndex ~= nil
 end
 
-local function GetFrame(index)
+local function CreateStateDriverAttribute(frame, state, condition)
+	frame:SetAttribute("_onstate-" .. state, [===[
+		if newstate == "enabled" then
+			self:RunAttribute("clicked-clear-binding")
+		else
+			self:RunAttribute("clicked-register-binding")
+		end
+	]===])
+
+	RegisterStateDriver(frame, state, condition)
+end
+
+local function GetFrameHandler(index)
 	if index > #macroFrameHandlers then
-		frame = CreateFrame("Button", "ClickedMacroFrameHandler" .. index, UIParent, "SecureActionButtonTemplate")
+		frame = CreateFrame("Button", "ClickedMacroFrameHandler" .. index, UIParent, "SecureActionButtonTemplate,SecureHandlerStateTemplate,SecureHandlerShowHideTemplate")
+		frame:Hide()
+
+		-- set required data first
+		frame:SetAttribute("clicked-registered", false)
+		frame:SetAttribute("clicked-can-enable", true)
+		frame:SetAttribute("clicked-keybind", "")
+		frame:SetAttribute("clicked-id", "")
+
+		-- register OnShow and OnHide handlers to ensure bindings are registered
+		frame:SetAttribute("_onshow", [[ self:RunAttribute("clicked-register-binding") ]])
+		frame:SetAttribute("_onhide", [[ self:RunAttribute("clicked-clear-binding") ]])
+
+		-- attempt to register a binding, this will also check if the binding
+		-- is currently allowed to be active (e.g. not in a vehicle or pet battle)
+		frame:SetAttribute("clicked-register-binding", [===[
+			if not self:IsShown() then
+				return
+			end
+
+			if self:GetAttribute("state-petbattle") == "enabled" then
+				return
+			end
+
+			if self:GetAttribute("state-vehicle") == "enabled" or self:GetAttribute("state-vehicleui") == "enabled" then
+				return
+			end
+
+			if self:GetAttribute("state-possessbar") == "enabled" then
+				return
+			end
+
+			local keybind = self:GetAttribute("clicked-keybind")
+			local id = self:GetAttribute("clicked-id")
+
+			self:SetBindingClick(true, keybind, self, id)
+		]===])
+
+		-- unregister a binding
+		frame:SetAttribute("clicked-clear-binding", [===[
+			local keybind = self:GetAttribute("clicked-keybind")
+			self:ClearBinding(keybind)
+		]===])
+
+		if Clicked.WOW_MAINLINE_RELEASE then
+			CreateStateDriverAttribute(frame, "vehicle", "[@vehicle,exists] enabled; disabled")
+			CreateStateDriverAttribute(frame, "vehicleui", "[vehicleui] enabled; disabled")
+			CreateStateDriverAttribute(frame, "possessbar", "[possessbar] enabled; disabled")
+			CreateStateDriverAttribute(frame, "petbattle", "[petbattle] enabled; disabled")
+		end
+
 		table.insert(macroFrameHandlers, frame)
 	end
 
@@ -68,17 +130,18 @@ function Clicked:ProcessCommands(commands)
 		end
 
 		if not isHoverCastBinding then
-			local frame = GetFrame(nextMacroFrameHandler)
+			local frame = GetFrameHandler(nextMacroFrameHandler)
 			local attributes = {}
 
 			nextMacroFrameHandler = nextMacroFrameHandler + 1
 
-			self:CreateCommandAttributes(attributes, command, "", "")
+			self:CreateCommandAttributes(attributes, command, prefix, suffix)
 			self:SetPendingFrameAttributes(frame, attributes)
 			self:ApplyAttributesToFrame(frame)
 
-			ClearOverrideBindings(frame)
-			SetOverrideBindingClick(frame, true, command.keybind, frame:GetName())
+			frame:SetAttribute("clicked-keybind", command.keybind)
+			frame:SetAttribute("clicked-id", suffix)
+			frame:Show()
 		end
 	end
 
@@ -88,8 +151,10 @@ function Clicked:ProcessCommands(commands)
 	for i = nextMacroFrameHandler, #macroFrameHandlers do
 		local frame = macroFrameHandlers[i]
 
-		self:ApplyAttributesToFrame(frame)
+		frame:Hide()
+		frame:SetAttribute("clicked-keybind", "")
+		frame:SetAttribute("clicked-id", "")
 
-		ClearOverrideBindings(frame)
+		self:ApplyAttributesToFrame(frame)
 	end
 end
