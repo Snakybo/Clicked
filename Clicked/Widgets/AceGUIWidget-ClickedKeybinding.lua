bc9e24e5ee43ad70ffda9e8d7f5e8f7ff6145299
Snakybo <krol.kevin@gmail.com>
2021-02-13 00:50:40 +0100

    Remove unused variable

--- Clicked/Widgets/AceGUIWidget-ClickedKeybinding.lua
+++ Clicked/Widgets/AceGUIWidget-ClickedKeybinding.lua
@@ -4,7 +4,6 @@ Set Keybindings in the Config UI.
 -------------------------------------------------------------------------------]]
 local Type, Version = "ClickedKeybinding", 2
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
-local L = LibStub("AceLocale-3.0"):GetLocale("Clicked")
 
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
