d9b864050c439e97f2e13424474638c6a7f27e27
Kevin Krol <krol.kevin@gmail.com>
2024-08-20 23:54:16 +0200

    Add status tab

--- Clicked/Config2/Tabs/Status.lua
+++ Clicked/Config2/Tabs/Status.lua
@@ -14,19 +14,163 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+local AceGUI = LibStub("AceGUI-3.0")
+local LibMacroSyntaxHighlight = LibStub("LibMacroSyntaxHighlight-1.0")
+
 --- @class ClickedInternal
 local Addon = select(2, ...)
 
 Addon.BindingConfig = Addon.BindingConfig or {}
 
 --- @class BindingConfigStatusTab : BindingConfigTab
+--- @field private selected integer
 Addon.BindingConfig.BindingStatusTab = {}
 
 function Addon.BindingConfig.BindingStatusTab:Show()
-end
-
-function Addon.BindingConfig.BindingStatusTab:Hide()
+	self.selected = 1
 end
 
 function Addon.BindingConfig.BindingStatusTab:Redraw()
+	if #self.bindings > 1 then
+		--- @param index integer
+		--- @return string
+		local function IndexToString(index)
+			return "item_" .. index
+		end
+
+		--- @param str string
+		--- @return integer
+		local function StringToIndex(str)
+			--- @diagnostic disable-next-line: return-type-mismatch
+			return tonumber(string.sub(str, 6))
+		end
+
+		local function OnValueChanged(_, _, value)
+			self.selected = StringToIndex(value)
+			self.controller:RedrawTab()
+		end
+
+		local items = {}
+		local order = {}
+
+		for i, binding in ipairs(self.bindings) do
+			local key = IndexToString(i)
+			local name, icon = Addon:GetBindingNameAndIcon(binding)
+			items[key] = Addon:GetTextureString(name, icon)
+			table.insert(order, key)
+		end
+
+		local dropdown = AceGUI:Create("Dropdown") --[[@as AceGUIDropdown]]
+		dropdown:SetLabel(Addon.L["Select binding from selection"])
+		dropdown:SetFullWidth(true)
+		dropdown:SetList(items, order)
+		dropdown:SetValue(IndexToString(self.selected))
+		dropdown:SetCallback("OnValueChanged", OnValueChanged)
+
+		self.container:AddChild(dropdown)
+	end
+
+	self:RedrawPage()
+end
+
+function Addon.BindingConfig.BindingStatusTab:RedrawPage()
+	local binding = self.bindings[self.selected]
+	if binding == nil then
+		return
+	end
+
+	local function DrawStatus(bindings, interactionType)
+		if #bindings == 0 then
+			return
+		end
+
+		local MAX_MACRO_LENGTH = 255
+
+		local text = Addon:GetMacroForBindings(bindings, interactionType)
+		local label = interactionType == Addon.InteractionType.HOVERCAST and Addon.L["Generated hovercast macro (%d/%d)"] or Addon.L["Generated macro (%d/%d)"]
+
+		label = string.format(label, strlenutf8(text), MAX_MACRO_LENGTH)
+
+		if #text > MAX_MACRO_LENGTH then
+			label = RED_FONT_COLOR:WrapTextInColorCode(label)
+		end
+
+		text = LibMacroSyntaxHighlight:Colorize(text)
+
+		local widget = AceGUI:Create("MultiLineEditBox") --[[@as AceGUIMultiLineEditBox]]
+		widget:SetLabel(label)
+		widget:SetText(text)
+		widget:SetFullWidth(true)
+		widget:SetNumLines(8)
+		widget:DisableButton(true)
+		widget:SetCallback("OnTextChanged", function()
+			widget:SetText(text)
+		end)
+
+		Addon.Media:UseMonoFont(widget)
+
+		self.container:AddChild(widget)
+	end
+
+	local hovercast = {}
+	local regular = {}
+	local all = {}
+
+	for _, other in Clicked:IterateActiveBindings() do
+		if other.keybind == binding.keybind then
+			local valid = false
+
+			if Addon:IsHovercastEnabled(binding) and Addon:IsHovercastEnabled(other) then
+				table.insert(hovercast, other)
+				valid = true
+			end
+
+			if Addon:IsMacroCastEnabled(binding) and Addon:IsMacroCastEnabled(other) then
+				table.insert(regular, other)
+				valid = true
+			end
+
+			if valid then
+				table.insert(all, other)
+			end
+		end
+	end
+
+	DrawStatus(hovercast, Addon.InteractionType.HOVERCAST)
+	DrawStatus(regular, Addon.InteractionType.REGULAR)
+
+	if #all > 1 then
+		table.sort(all, function (left, right)
+			return Addon:GetBindingNameAndIcon(left) < Addon:GetBindingNameAndIcon(right)
+		end)
+
+		do
+			local widget = AceGUI:Create("Heading") --[[@as AceGUIHeading]]
+			widget:SetFullWidth(true)
+			widget:SetText(Addon.L["%d related binding(s)"]:format(#all - 1))
+
+			self.container:AddChild(widget)
+		end
+
+		for _, other in ipairs(all) do
+			if other ~= binding then
+				do
+					local function OnClick()
+						-- TODO: Select binding
+					end
+
+					local name, icon = Addon:GetBindingNameAndIcon(other)
+
+					local widget = AceGUI:Create("InteractiveLabel") --[[@as AceGUIInteractiveLabel]]
+					widget:SetFontObject(GameFontHighlight)
+					widget:SetText(name)
+					widget:SetImage(icon)
+					widget:SetFullWidth(true)
+					widget:SetCallback("OnClick", OnClick)
+
+					self.container:AddChild(widget)
+				end
+			end
+		end
+	end
 end
